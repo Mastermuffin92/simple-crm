4bcd5dd181c9febcaecd59669951e1ed8fb41e8a
Marvin Wiemers <marvinwiemers@gmail.com>
2022-03-17 14:54:37 +0100

    update edit function

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -36,13 +36,15 @@ export class UserDetailComponent implements OnInit {
   }
   editMenu(){
     const dialog =  this.dialog.open(DialogEditAddressComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
+    dialog.componentInstance.userId = this.userId;
   }
 
 
   editUserDetail(){
     const dialog = this.dialog.open(DialogEditUserComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
+    dialog.componentInstance.userId = this.userId;
   }
 
 
